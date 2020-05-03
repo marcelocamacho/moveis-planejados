9b2fb31edeed38451d20ad7053019d552ad5b67f
Marcelo Santana Camacho <marcelocamacho.ufpa@gmail.com>
2020-05-03 09:56:05 -0300

    Componetizando a Logomarca do site

---
+++ src/components/Logo.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Logo = ()=>{
+	return(
+		<div className='text-white'>
+				<h1 className='font-bold text-2xl'>Lima moveis</h1>
+				<p>Moveis planejados</p>
+		</div>
+	)
+};
+
+export default Logo;
