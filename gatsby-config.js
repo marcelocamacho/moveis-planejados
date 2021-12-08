cf1cdd99330f2c7f3326f8832045ce8dabdd3e5d
Marcelo S. Camacho <marcelocamacho.ufpa@gmail.com>
2021-12-08 14:27:12 -0300

    Add de recurso Analytics com gatsby-plugin-google-analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -12,5 +12,13 @@ module.exports = {
 			}
 		},
 		'gatsby-plugin-react-helmet',
+		{
+      			resolve: `gatsby-plugin-google-analytics`,
+      			options: {
+        			// The property ID; the tracking code won't be generated without it
+        		trackingId: "G-Q3STZ8F7NY",
+      			},
+    		},
 ]
 }
+//<meta name="google-site-verification" content="J0g4XOzuu0HirNv2QB2wj-Enhp50-y-tJVxfMHNroHk" />
